df3dafd14d4d6bd5384bb9363f93e2d9b4e491e2
Mark Richman <mark@markrichman.com>
2019-01-16 16:55:25 -0500

    Added lab files

---
+++ Lab-Create-an-EC2-Instance-Using-Lambda/lambda_execution_role.json
@@ -0,0 +1,20 @@
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    },
+    {
+      "Action": [
+        "ec2:RunInstances"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
---
+++ Lab-Create-an-EC2-Instance-Using-Lambda/lambda_function.py
@@ -0,0 +1,23 @@
+import os
+import boto3
+
+AMI = os.environ['AMI']
+INSTANCE_TYPE = os.environ['INSTANCE_TYPE']
+KEY_NAME = os.environ['KEY_NAME']
+SUBNET_ID = os.environ['SUBNET_ID']
+
+ec2 = boto3.resource('ec2')
+
+
+def lambda_handler(event, context):
+
+    instance = ec2.create_instances(
+        ImageId=AMI,
+        InstanceType=INSTANCE_TYPE,
+        KeyName=KEY_NAME,
+        SubnetId=SUBNET_ID,
+        MaxCount=1,
+        MinCount=1
+    )
+
+    print("New instance created:", instance[0].id)
