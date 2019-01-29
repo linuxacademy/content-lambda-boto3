2906580b461725922ab323e336221cac86382c47
Mark Richman <mark@markrichman.com>
2019-01-29 11:26:37 -0500

    Added

---
+++ EC2/Stopping-EC2-Instances-Nightly/lambda_function.py
@@ -0,0 +1,25 @@
+import boto3
+
+
+def lambda_handler(event, context):
+
+    # Get list of regions
+    ec2_client = boto3.client('ec2')
+    regions = [region['RegionName']
+               for region in ec2_client.describe_regions()['Regions']]
+
+    # Iterate over each region
+    for region in regions:
+        ec2 = boto3.resource('ec2', region_name=region)
+
+        print("Region:", region)
+
+        # Get only running instances
+        instances = ec2.instances.filter(
+            Filters=[{'Name': 'instance-state-name',
+                      'Values': ['running']}])
+
+        # Stop the instances
+        for instance in instances:
+            instance.stop()
+            print('Stopped instance: ', instance.id)
