cf6c7f20ef11be5ff434df8d59d3aefc83ce633b
Mark Richman <mark@markrichman.com>
2019-02-18 14:17:37 -0500

    Update

--- S3/Transcribing-Audio/TranscribeAudio/lambda_function.py
+++ S3/Transcribing-Audio/TranscribeAudio/lambda_function.py
@@ -9,11 +9,12 @@ def lambda_handler(event, context):
     for record in event['Records']:
         source_bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
-        object_url = "https://s3.amazonaws.com/{1}/{2}".format(
+        object_url = "https://s3.amazonaws.com/{0}/{1}".format(
             source_bucket, key)
         response = transcribe.start_transcription_job(
-            TranscriptionJobName=key,
+            TranscriptionJobName='MyTranscriptionJob',
             Media={'MediaFileUri': object_url},
             MediaFormat='mp3',
+            LanguageCode='en-US'
         )
         print(response)
