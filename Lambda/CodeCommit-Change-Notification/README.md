3c86048d751627530e3c059894ba476e163a33f3
Mark Richman <mark@markrichman.com>
2019-03-15 11:53:28 -0400

    Automating CodeCommit Change Notifications

---
+++ Lambda/CodeCommit-Change-Notification/README.md
@@ -0,0 +1,73 @@
+# Automating CodeCommit Change Notifications
+
+In this lesson, we'll demonstrate how to receive detailed email notifications
+about file changes and commit messages when a code update is pushed to
+CodeCommit. A code reviewer may subscribe to the SNS topic and recieve updates
+for any changes.
+
+## Create the CodeCommit Repository
+
+```sh
+aws codecommit create-repository --repository-name ChangeNotification
+```
+
+Note the `cloneUrlHttp` and `Arn` values in the response.
+
+## Create and Subscribe to the SNS Topic
+
+## Create IAM Lambda Execution Role
+
+1. Add `AWSLambdaBasicExecutionRole`
+2. Add the following policy `LambdaCodeCommitSnsPolicy`:
+
+    ```json
+    {
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Effect": "Allow",
+            "Action": [
+            "codecommit:*",
+            "sns:*"
+            ],
+            "Resource": "*"
+        }]
+    }
+    ```
+
+## Create the Lambda Function
+
+Name: **CodeCommitChangeNotification**
+
+## Create the CloudWatch Event Rule
+
+This rule will detect branch or repository changes:
+
+1. Choose **Event Pattern**.
+2. Service Name: **CodeCommit**
+3. Event Type: **CodeCommit Repository State Change**
+4. Specific resource(s) by ARN: **CodeCommit Repository ARN**
+
+    Select the **referenceCreated** and **referenceUpdated** events.
+
+    Event Pattern:
+
+    ```json
+    {
+    "source": [
+        "aws.codecommit"
+    ],
+    "detail-type": [
+        "CodeCommit Repository State Change"
+    ],
+    "resources": [
+        "arn:aws:codecommit:us-east-1:123456789012:ChangeNotification"
+    ]
+    }
+    ```
+
+5. Target: **CodeCommitChangeNotification**
+
+## Commit a Change
+
+1. Create and commit a file.
+2. Edit the file and commit it.
---
+++ Lambda/CodeCommit-Change-Notification/lambda_function.py
@@ -0,0 +1,143 @@
+import datetime
+import json
+import os
+
+import boto3
+
+AWS_DEFAULT_REGION = os.environ["AWS_DEFAULT_REGION"]
+MAIN_BRANCH_NAME = os.getenv('MAIN_BRANCH_NAME', 'master')
+REPOSITORY_NAME = os.environ['REPOSITORY_NAME']
+SNS_TOPIC_ARN = os.environ['SNS_TOPIC_ARN']
+
+codecommit = boto3.client('codecommit')
+sns = boto3.client('sns')
+
+
+def get_diff(repository_name, last_commit_id, previous_commit_id):
+    response = None
+
+    if previous_commit_id is not None:
+        response = codecommit.get_differences(
+            repositoryName=repository_name,
+            beforeCommitSpecifier=previous_commit_id,
+            afterCommitSpecifier=last_commit_id
+        )
+    else:
+        # This was the first commit (no previous, omit beforeCommitSpecifier)
+        response = codecommit.get_differences(
+            repositoryName=repository_name,
+            afterCommitSpecifier=last_commit_id
+        )
+
+    differences = []
+
+    if response is None:
+        return differences
+
+    while "nextToken" in response:
+        response = codecommit.get_differences(
+            repositoryName=repository_name,
+            beforeCommitSpecifier=previous_commit_id,
+            afterCommitSpecifier=last_commit_id,
+            nextToken=response["nextToken"]
+        )
+        differences += response.get("differences", [])
+    else:
+        differences += response["differences"]
+
+    return differences
+
+
+def get_diff_change_message_type(change_type):
+    type = {
+        'M': 'Modification',
+        'D': 'Deletion',
+        'A': 'Addition'
+    }
+    return type[change_type]
+
+
+def get_last_commit_id(repository, branch):
+    response = codecommit.get_branch(
+        repositoryName=repository,
+        branchName=branch
+    )
+    commit_id = response['branch']['commitId']
+    return commit_id
+
+
+def get_last_commit_log(repository, commit_id):
+    response = codecommit.get_commit(
+        repositoryName=repository, commitId=commit_id)
+
+    return response['commit']
+
+
+def get_message_text(differences, last_commit):
+    print(last_commit)
+    text = ''
+    commit_id = last_commit['commitId']
+    text += f'commit ID: {commit_id}\n'
+    text += 'author: {0} ({1})\n'.format(
+        last_commit['author']['name'],
+        last_commit['author']['email'])
+    date = last_commit['author']['date'].split()[0]
+    t_utc = datetime.datetime.utcfromtimestamp(int(date))
+    timestamp = t_utc.strftime('%Y-%m-%d %H:%M:%S UTC')
+    text += f'date: {timestamp}\n'
+    text += f"message: {last_commit['message']}\n"
+    for diff in differences:
+        if 'afterBlob' in diff:
+            text += 'After - File: {0} {1} - Blob ID: {2}\n'.format(
+                diff['afterBlob']['path'],
+                get_diff_change_message_type(diff['changeType']),
+                diff['afterBlob']['blobId'])
+        if 'beforeBlob' in diff:
+            text += 'Before - File: {0} {1} - Blob ID: {2}\n'.format(
+                diff['beforeBlob']['path'],
+                get_diff_change_message_type(diff['changeType']),
+                diff['beforeBlob']['blobId'])
+
+    text += (f'Commit: https://{AWS_DEFAULT_REGION}.console.aws.amazon.com/codesuite/'
+             f'codecommit/repositories/{REPOSITORY_NAME}/commit/{commit_id}?'
+             f'region={AWS_DEFAULT_REGION}')
+
+    return text
+
+
+def publish(repository, message):
+    sns.publish(
+        TopicArn=SNS_TOPIC_ARN,
+        Subject='CodeCommit Update - Repository: {0}'.format(repository),
+        Message=message
+    )
+
+
+def lambda_handler(event, context):
+    print(json.dumps(event))
+
+    repository = REPOSITORY_NAME
+
+    try:
+        last_commit_id = get_last_commit_id(repository, MAIN_BRANCH_NAME)
+        last_commit = get_last_commit_log(repository, last_commit_id)
+
+        previous_commit_id = None
+        if len(last_commit['parents']) > 0:
+            previous_commit_id = last_commit['parents'][0]
+
+        print(f'Last commit ID: {last_commit_id}')
+        print(f'Previous commit ID: {previous_commit_id}')
+
+        differences = get_diff(repository, last_commit_id, previous_commit_id)
+        message_text = get_message_text(differences, last_commit)
+
+        return publish(repository, message_text)
+
+    except Exception as e:
+        import traceback
+        print(e)
+        traceback.print_exc()
+        print(f'Error getting repository {repository}. Ensure it exists in the '
+              'same region as this function.')
+        raise e
