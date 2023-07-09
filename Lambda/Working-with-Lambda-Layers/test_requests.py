05c0e03aaf8b0e972cd80c3a02705fa4bbb5242f
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-07-09 13:05:25 -0700

    Update test_requests.py

--- Lambda/Working-with-Lambda-Layers/test_requests.py
+++ Lambda/Working-with-Lambda-Layers/test_requests.py
@@ -1,3 +1,6 @@
+# make an HTTP GET request to the provided URL and determine if the request was successful. If the request was successful, it prints 'Success!', and if an error occurred or the response was not valid, it prints 'An error has occurred.'.
+# This code is intended to be used as an AWS Lambda function that can be triggered by an event or schedule. 
+
 import requests
 
 
